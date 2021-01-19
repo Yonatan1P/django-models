585c3970a9ff8661c65bc3e821c76430d1d5946e
Yonatan Palagashvili <yonimotek@gmail.com>
2021-01-18 23:10:40 -0800

    renamed Blog to Post models and made a body property rather than a rank property

--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.contrib.auth import get_user_model
 # Create your models here.
 
-class Blog(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=64)
-    rank = models.IntegerField(default=100)
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    body = models.CharField(max_length=144)
 
     def __str__(self):
         return self.title
--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from .views import HomeView, AboutView, BlogDetailView
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('about/', AboutView.as_view(), name='about'),
-    path('details/<int:pk>/', BlogDetailView.as_view(), name="blog_detail"),
+    path('details/<int:pk>/', BlogDetailView.as_view(), name="post_detail"),
 ]
