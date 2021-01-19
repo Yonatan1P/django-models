a0a5983500a828aa04b1155d6d2f2ab66c19c069
Yonatan Palagashvili <yonimotek@gmail.com>
2021-01-18 23:02:19 -0800

    made the blog project and the blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=64)
+    rank = models.IntegerField(default=100)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Blog
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+    model = Blog
 
 class AboutView(TemplateView):
     template_name = 'about.html'
+
+class BlogDetailView(DetailView):
+    template_name = 'blog-detail.html'
+    model = Blog
